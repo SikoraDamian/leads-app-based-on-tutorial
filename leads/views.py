be86d6812e2dfbc3e4613e589f3b6d59ec182af7
SikoraDamian <sikoradblog@gmail.com>
2021-02-28 18:22:39 +0000

    function based views removed

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,4 @@
+from django.core.mail import send_mail
 from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 from .models import Lead, Agent
@@ -11,24 +12,12 @@ class LeadListView(ListView):
     template_name = "leads/lead_list.html"
     queryset = Lead.objects.all()
 
-def lead_list(request):
-    leads = Lead.objects.all()
-    context = {
-        "leads": leads
-    }
-    return render(request, "leads/lead_list.html", context)
 
 class LeadDetailView(DetailView):
     template_name = "leads/lead_detail.html"
     queryset = Lead.objects.all()
     context_object_name = "lead"
 
-def lead_detail(request, pk):
-    lead = Lead.objects.get(id=pk)
-    context = {
-        "lead": lead
-    }
-    return render(request, "leads/lead_detail.html", context)
 
 class LeadCreateView(CreateView):
     template_name = "leads/lead_create.html"
@@ -36,18 +25,10 @@ class LeadCreateView(CreateView):
     
     def get_success_url(self):
         return reverse('leads:leads-list')
+    
+    def form_valid(self, form):
+        return super(LeadCreateView, self).form_valid(form)
 
-def lead_create(request):
-    form = LeadModelForm()
-    if request.method == "POST":
-        form = LeadModelForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("/")
-    context = {
-        "form": form
-    }
-    return render(request, "leads/lead_create.html", context)
 
 class LeadUpdateView(UpdateView):
     template_name = "leads/lead_update.html"
@@ -57,19 +38,6 @@ class LeadUpdateView(UpdateView):
     def get_success_url(self):
         return reverse('leads:leads-list')
 
-def lead_update(request, pk):
-    lead = Lead.objects.get(id=pk)
-    form = LeadModelForm(instance=lead)
-    if request.method == "POST":
-        form = LeadModelForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("/")
-    context = {
-        "form": form,
-        "lead": lead
-    }
-    return render(request, "leads/lead_update.html", context)
 
 class LeadDeleteView(DeleteView):
     template_name = "leads/lead_delete.html"
@@ -77,9 +45,3 @@ class LeadDeleteView(DeleteView):
     
     def get_success_url(self):
         return reverse('leads:leads-list')
-
-
-def lead_delete(request, pk):
-    lead = Lead.objects.get(id=pk)
-    lead.delete()
-    return redirect('/')
