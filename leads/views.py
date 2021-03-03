f9ae018250dade117b3d995064664d427ff72c1c
SikoraDamian <sikoradblog@gmail.com>
2021-03-03 16:11:46 +0000

    user profile done

--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,6 @@
 from django.core.mail import send_mail
-
 from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 from .models import Lead, Agent
 from .forms import LeadForm, LeadModelForm, CustomUserCreationForm
@@ -9,12 +9,11 @@ from django.views.generic import TemplateView, ListView, DetailView, CreateView,
 class LandingPagesView(TemplateView):
     template_name = "landing.html"
 
-class LeadListView(ListView):
+class LeadListView(LoginRequiredMixin, ListView):
     template_name = "leads/lead_list.html"
     queryset = Lead.objects.all()
 
-
-class LeadDetailView(DetailView):
+class LeadDetailView(LoginRequiredMixin, DetailView):
     template_name = "leads/lead_detail.html"
     queryset = Lead.objects.all()
     context_object_name = "lead"
@@ -26,7 +25,7 @@ class SignupView(CreateView):
     def get_success_url(self):
         return reverse('leads:leads-list')
 
-class LeadCreateView(CreateView):
+class LeadCreateView(LoginRequiredMixin, CreateView):
     template_name = "leads/lead_create.html"
     form_class = LeadModelForm
     
@@ -42,8 +41,7 @@ class LeadCreateView(CreateView):
         )
         return super(LeadCreateView, self).form_valid(form)
 
-
-class LeadUpdateView(UpdateView):
+class LeadUpdateView(LoginRequiredMixin, UpdateView):
     template_name = "leads/lead_update.html"
     form_class = LeadModelForm
     queryset = Lead.objects.all()
@@ -51,8 +49,7 @@ class LeadUpdateView(UpdateView):
     def get_success_url(self):
         return reverse('leads:leads-list')
 
-
-class LeadDeleteView(DeleteView):
+class LeadDeleteView(LoginRequiredMixin, DeleteView):
     template_name = "leads/lead_delete.html"
     queryset = Lead.objects.all()
     
