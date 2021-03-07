69a5ea980e5264e361748e2845872978e327f62d
SikoraDamian <sikoradblog@gmail.com>
2021-03-07 19:20:57 +0000

    passwort restart

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,6 +1,13 @@
 from django.conf import settings
 from django.contrib import admin
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView
+)
 from django.urls import path, include
 from django.conf.urls.static import static
 from leads.views import LandingPagesView, SignupView
@@ -12,6 +19,10 @@ urlpatterns = [
     path('agents/', include('agents.urls', namespace='agents')),
     path('signup/', SignupView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
+    path('password_reset/', PasswordResetView.as_view(), name='password-reset'),
+    path('password_reset_done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset_complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password_reset_confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('', LandingPagesView.as_view(), name='landing-page'),
 ]
