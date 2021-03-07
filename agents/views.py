97de516ed00022824692b45ed2cc9a87753f8732
SikoraDamian <sikoradblog@gmail.com>
2021-03-07 14:47:11 +0000

    invite agent done

--- agents/views.py
+++ agents/views.py
@@ -1,4 +1,7 @@
+import random
+
 from django.views import generic
+from django.core.mail import send_mail
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import reverse
 from leads.models import Agent
@@ -21,9 +24,21 @@ class AgentCreateView(OrganisorAndLoginRequiredMixin, generic.CreateView):
         return reverse("agents:agent-list")
 
     def form_valid(self, form):
-        agent = form.save(commit=False)
-        agent.organisation = self.request.user.userprofile
-        agent.save()
+        user = form.save(commit=False)
+        user.is_agent = True
+        user.is_organisor = False
+        user.set_password(f"{random.random(0, 10000)}")
+        user.save()
+        Agent.objects.create(
+            user=user,
+            organisation=self.request.user.userprofile,
+        )
+        send_mail(
+            subject='You are invited to be an agent',
+            message='You are added as an agent on DJCRM. Please come login to start working.',
+            from_email='admin@admin.com',
+            recipient_list=[user.email]
+        )
         return super(AgentCreateView, self).form_valid(form)
 
 class AgentDetailView(OrganisorAndLoginRequiredMixin, generic.DetailView):
